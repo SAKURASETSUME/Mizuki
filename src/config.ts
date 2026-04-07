c8c97771de013940aadd98b2db8a1086abaa2690
NatsumeRin <1449196676@qq.com>
2026-04-07 22:39:03 +0800

    修改配置文件 取消了设备 更改了关于页面

--- src/config.ts
+++ src/config.ts
@@ -46,8 +46,8 @@ export const siteConfig: SiteConfig = {
 		projects: false, // 项目页面开关
 		skills: false, // 技能页面开关
 		timeline: true, // 时间线页面开关
-		albums: false, // 相册页面开关
-		devices: true, // 设备页面开关
+		albums: true, // 相册页面开关
+		devices: false, // 设备页面开关
 	},
 
 	// 顶栏标题配置
